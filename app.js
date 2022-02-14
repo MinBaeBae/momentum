58d2558b1c621f6ca4b650c70d4ec910d04735a6
MinBaeBae <93083003+MinBaeBae@users.noreply.github.com>
2022-02-14 15:59:01 +0900

    login form

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greetings = document.querySelector("#greetings");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greetings.innerText = `Hello ${username} What's good mang?`;
-  greetings.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+//show the greetings
+greetings.innerText = `Hello ${username} What's good mang?`;
+greetings.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
-const savedUsername = localStorage.getItem("username");
+if(savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+  }
